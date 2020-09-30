31ac2d8ce140fff9ac23d407720aa0670afc9b88
OOF\kiera <kieranjayes@hotmail.com>
2020-09-30 15:52:02 +0100

    Main

---
+++ Cinderkapsule/Cinderkapsule/Main.cpp
@@ -0,0 +1,16 @@
+#include "Main.h"
+
+int main(int argc, char* argv[])
+{
+	HelloTriangleApplication app;
+
+	try {
+		app.run();
+	}
+	catch (const std::exception & e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ Cinderkapsule/Cinderkapsule/compile.bat
@@ -0,0 +1,3 @@
+C:/VulkanSDK/1.2.148.1/Bin32/glslc.exe vertex_shader.glsl -o vert.spv
+C:/VulkanSDK/1.2.148.1/Bin32/glslc.exe fragment_shader.glsl -o frag.spv
+pause
