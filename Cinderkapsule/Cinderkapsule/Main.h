d012b272ac5f37f5795ea202a4b144d63fb6e33e
DESKTOP-G8ID3C9\kiera <kieranjayes@hotmail.com>
2020-11-12 03:23:26 +0000

    Removed extraneous folders

--- Cinderkapsule/Cinderkapsule/Main.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "VulkanImpl.h"
-#include "Sound.h"
-#include "Player.h"
-#include <glm/gtx/quaternion.hpp>
-#include "Camera.h"
-#include <thread>
-
-void gameLoop(VulkanImpl* vulkan, SoundManager* soundManager, Camera* camera);
